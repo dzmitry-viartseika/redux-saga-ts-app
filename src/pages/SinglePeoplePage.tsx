d12ea218e13a94cbb6ab3dfa8c6d1bc75cd1baf8
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-05-17 17:27:25 +0300

    added types for user-details

--- src/pages/SinglePeoplePage.tsx
+++ src/pages/SinglePeoplePage.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import {selectUserDetail} from "../redux/selectors/selectors";
 
 function SinglePeoplePage(): JSX.Element {
-    const { id } = useParams();
+    const { id } = useParams<string>();
     const dispatch = useDispatch();
     const userDetail = useSelector(selectUserDetail);
 
